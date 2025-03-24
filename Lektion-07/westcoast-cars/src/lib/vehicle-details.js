fe0a106d077adb3f8538f8c9fbdae30bd7f64b61
MichaelGustavsson <michael.gustavsson@skaftoskorpan.se>
2025-03-24 14:59:30 +0100

    Updated Westcoast Cars

--- Lektion-07/westcoast-cars/src/lib/vehicle-details.js
+++ Lektion-07/westcoast-cars/src/lib/vehicle-details.js
@@ -3,7 +3,16 @@ import { vehicles } from '../data/vehicles.js';
 const initApp = () => {
   console.log(location.search);
   const vehicleId = location.search.split('=')[1];
-  console.log(vehicleId);
+  console.log(location.search.split('='));
+  console.log(vehicleId, typeof vehicleId);
+  loadVehicle(vehicleId);
+};
+
+const loadVehicle = (vehicleId) => {
+  // Hur hittar vi en bil med inskickat id?
+  // const vehicle = vehicles.find((v) => v.id === +vehicleId);
+  const vehicle = vehicles.find((v) => v.id === parseInt(vehicleId));
+  console.log(vehicle);
 };
 
 document.addEventListener('DOMContentLoaded', initApp);
