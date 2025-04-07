ca2624f7dfc737787f590e459cee620263f9a1d6
MichaelGustavsson <michael.gustavsson@skaftoskorpan.se>
2025-04-07 16:57:07 +0200

    Added solution for lesson 11

--- Lektion-11/MovieFlix/src/lib/helpers/httpClient.js
+++ Lektion-11/MovieFlix/src/lib/helpers/httpClient.js
@@ -17,3 +17,20 @@ export const get = async (endpoint) => {
     console.error(error);
   }
 };
+
+export const search = async (endpoint, searchCriteria) => {
+  const url = `${config.apiUrl}/search/${endpoint}?api_key=${config.apiKey}&query=${searchCriteria}`;
+
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw new Error(
+        `Något gick fel ${response.status}, ${response.statusText}`
+      );
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
